cd18487cf7829f08900bfb4e79b62d4f1d087ea2
Lastro21 <ptokarev8@gmail.com>
2019-07-22 09:05:45 -0700

    Add try/catch

--- src/App.js
+++ src/App.js
@@ -18,31 +18,40 @@ class App extends Component {
 
     gettingInfoFromOutAPI = async (e) => {
 
-        this.setState({
-            color: "blue"
-        });
-
         e.preventDefault();
 
         var two_part = e.target.elements.route.value;
 
-        const api_url = await fetch(`http://localhost:8090/${one_path}/${two_part}`).then(response => response.json());
-        console.log(api_url.name);
 
-        if (two_part.length > 0) {
+        try {
+            const api_url = await fetch(`http://localhost:8090/${one_path}/${two_part}`).then(response => response.json());
+            console.log(api_url.name);
+            console.log(api_url.age);
+
             this.setState({
-                name: api_url.name,
-                age: api_url.age,
-                error: undefined,
-                // color: "blue"
+                color: "blue"
             });
-        } else {
-            this.setState({
-                name: undefined,
-                age: undefined,
-                error: "Error: empty input.",
-                // color: "blue"
-            });
+
+            if (two_part.length > 0) {
+                this.setState({
+                    name: api_url.name,
+                    age: api_url.age,
+                    error: undefined,
+                    // color: "blue"
+                });
+            } else {
+                this.setState({
+                    name: undefined,
+                    age: undefined,
+                    error: "Error: empty input.",
+                    // color: "blue"
+                });
+            }
+
+            console.log();
+
+        } catch (e) {
+            console.log("Not answer from server. Reason: " + e);
         }
 
 
@@ -51,9 +60,9 @@ class App extends Component {
     render() {
         return (
             <div id="mainWrapper">
-                <Info my_color={this.state.color} />
-                <Form getInfoApiMethod={this.gettingInfoFromOutAPI} />
-                <User name={this.state.name} age={this.state.age} error={this.state.error} />
+                <Info my_color={this.state.color}/>
+                <Form getInfoApiMethod={this.gettingInfoFromOutAPI}/>
+                <User name={this.state.name} age={this.state.age} error={this.state.error}/>
             </div>
         );
     }
