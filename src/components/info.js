3fb17eab0303018fbdb8dae672b9babfcd6eed2c
Lastro21 <ptokarev8@gmail.com>
2019-07-23 01:54:11 -0700

    Add right css (without sass)

--- src/components/info.js
+++ src/components/info.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
+import './info.css';
 
 class Info extends Component {
     render() {
         return (
             <div>
-                <p id="first_p" style={{color : this.props.my_color}}>First app</p>
-                <p>Hello world!!!</p>
+                <p id="first_app1" style={{color : this.props.my_color}}>First app</p>
+                <p id="hello_world">Hello world!!!</p>
+                <p className="first_app">sfwe2222115</p>
             </div>
         );
     }
